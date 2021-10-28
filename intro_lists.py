952833a5d12c4986f5fd0f4a87cd9c1fc2fc6b51
AFishyOcean <dljhazelh@gmail.com>
2021-10-28 09:26:58 -0400

    worked on lists

--- intro_lists.py
+++ intro_lists.py
@@ -4,8 +4,10 @@ def swap(list_one):
     :param list_one: a list of at least two elements
     :return: the same list with the first and last elements swapped
     """
-    pass # make sure to remove this line before beginning work on this function
-
+    x = list_one[0]
+    list_one[0] = list_one[-1]
+    list_one[-1] = x
+    return list_one
 
 def rotate_left(list_one):
     """
@@ -13,7 +15,7 @@ def rotate_left(list_one):
     :param list_one: A list consisting of exactly three integers
     :return: a list where all the elements have been shifted 1 place to the left
     """
-    pass # make sure to remove this line before beginning work on this function
+
 
 
 def max_end(list_one):
@@ -24,3 +26,7 @@ def max_end(list_one):
     :return: A list where all the elements are the larger of the first or last element of the original list
     """
     pass # make sure to remove this line before beginning work on this function
+
+
+list_one = [1, 2, 3]
+swap(list_one)
--- list_slicing.py
+++ list_slicing.py
@@ -9,33 +9,33 @@ def main():
 
     # 1. Print ['Doug', 'Emma']
 
-
+    print(names[3:5])
 
     # 2. Print [‘Brenda’, ‘Chad’, ‘Doug’, ‘Emma’, ‘Francis’]
 
-
+    print(names[1:6])
 
     # 3. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using two numbers in the slice)
-
+    print(names[5:12])
 
     # 4. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using one number in the slice)
-
+    print(names[5:])
 
 
     # 5. [‘Linda’] (using a positive number)
-
+    print(names[11])
 
     # 6. [‘Linda’] (using a negative number)
-
+    print(names[-1])
 
 
     # 7. [‘Brenda’, ‘Doug’, ‘Francis’, ‘Harold’]
 
-
+    print(names[1:8:2])
 
     # 8. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’]
 
-
+    print(names[0::2])
 
     # 9. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’] (using a different way than above.
     # HINT: If you leave a slice number blank, the default is either the beginning or the end of the list,
@@ -44,15 +44,15 @@ def main():
 
     # 10. [‘Linda’, ‘Kurt’, ‘Jackie’]
 
-
+    print(names[-1:-4:-1])
     # 11. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
 
-
+    print(names[-1:-12:-1])
 
     # 12. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
     # (in a different way than 11. See hint for 9 for help.)
 
-
+    print(names[-1::-1])
 
 
 
