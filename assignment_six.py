54d50718ae8145edca93a11afcce6c99be05c6c2
AFishyOcean <dljhazelh@gmail.com>
2021-11-08 08:50:20 -0500

    worked on assignment six

--- assignment_six.py
+++ assignment_six.py
@@ -1,8 +1,17 @@
 def get_max():
+    """
+    gets the highest number for the lists
+    :return:
+    """
     x = int(input("What is the highest number you would like to find primes bellow"))
     return x
 
 def gen_list(x):
+    """
+    generates list of numbers from 2 to the max number
+    :param x:
+    :return:
+    """
     t = 1
     list_one = []
     while t < x:
@@ -10,20 +19,31 @@ def gen_list(x):
         list_one.append(t)
     return list_one
 
-def primes(list_one, x):
+def primes(list_one):
+    """
+    Finds the numbers that aren't prime and deletes them
+    adds the primes from list one to list two
+    :param list_one:
+    :return:
+    """
     list_two = []
-    size = len(list_one)
-    while size > 0:
-        if list_one[:] % list_one[0] > 0:
-            list_two.append(list_one[])
-            list_one.pop[]
-            size = len(list_one)
+    while len(list_one) > 0:
+        testing = list_one[0]
+        list_two.append(list_one[0])
+        for x in range(len(list_one)):
+            if list_one[x] % testing == 0:
+                list_one.pop(x)
+    return list_two
 
 def main():
+    """
+    calls all the functions
+    :return:
+    """
     x = get_max()
     list_one = gen_list(x)
     print("Original list:", list_one)
-    print("Primes list:", primes(list_one, x))
+    print("Primes list:", primes(list_one))
 
 
 
